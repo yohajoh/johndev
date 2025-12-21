994666125a7c69d67ffa8877b11e641422cecaa3
yohannes belete <ybelete490@gmail.com>
2025-12-21 11:41:36 -0500

    Fix netlify build

--- lib/constants.ts
+++ lib/constants.ts
@@ -151,14 +151,6 @@ export const SOCIAL_LINKS = [
       "bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600",
     textColor: "text-white",
   },
-  {
-    platform: "Email",
-    url: "mailto:ybelete490@gmail.com",
-    icon: Mail,
-    label: "Send Email",
-    color: "bg-green-600 hover:bg-green-500",
-    textColor: "text-white",
-  },
 ] as const;
 
 // Skill categories
